083288ba31898c60441220159d7bb13ea67c8b16
aatif <aatifmdalam@gmail.com>
2025-07-04 20:36:00 +0530

    added no image in top nav

--- src/components/templates/TopNav.jsx
+++ src/components/templates/TopNav.jsx
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "../../utils/axios";
+import noimage from "/noimage.png"
 
 const TopNav = () => {
   let [query, setQuery] = useState("");
-  let [searches, setSearches] = useState("null");
+  let [searches, setSearches] = useState([]);
 
   const GetSearches = async () => {
     try {
       const { data } = await axios.get(`/search/multi?query=${query}`);
       setSearches(data.results);
+      console.log(data.results);
     } catch (error) {
       console.log("Error: ", error);
     }
@@ -29,13 +31,14 @@ const TopNav = () => {
         type="text"
         placeholder="Search..."
       />
+      {/* to show the cross marks when user search */}
       {query.length > 0 && (
         <i
           onClick={() => setQuery("")}
-          class="ri-close-fill text-3xl text-zinc-400"
+          className="ri-close-fill text-3xl text-zinc-400"
         ></i>
       )}
-      {/* to show the cross marks when user search */}
+
       {/* search box hint */}
       <div className="absolute w-[50%] max-h-[50vh] bg-zinc-200 top-[90%] overflow-auto rounded">
         {searches.map((s, i) => (
@@ -43,8 +46,19 @@ const TopNav = () => {
             key={i}
             className="hover:text-black hover:bg-zinc-300 font-semibold text-zinc-600 w-[100%] p-10 flex justify start items-center border-b-2 border-zinc-100"
           >
-            <img src="" alt="" />
-            <span>{s.name || s.title || s.original_name || s.original_title  }</span>
+            <img
+              className="w-[10vh] h-[10vh] object-cover rounded mr-5 shadow-md"
+              src={
+                s.backdrop_path || 
+                s.profile_path ?`https://image.tmdb.org/t/p/original/${
+                s.backdrop_path || s.profile_path
+              }` : noimage
+            }
+              alt=""
+            />
+            <span>
+              {s.name || s.title || s.original_name || s.original_title}
+            </span>
           </Link>
         ))}
       </div>
