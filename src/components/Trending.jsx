2deba6915f23a2786bab1c64f38ec569dee9e960
aatif <aatifmdalam@gmail.com>
2025-07-13 23:08:33 +0530

    removed repetition from trending card in infinite scroll

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -4,36 +4,50 @@ import TopNav from "./templates/TopNav";
 import Dropdown from "./templates/Dropdown";
 import axios from "../utils/axios.jsx";
 import Cards from "./templates/Cards";
-import Loading from "./Loading.jsx"
-import InfiniteScroll from 'react-infinite-scroll-component';
+import Loading from "./Loading.jsx";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const Trending = () => {
-
-   // function to navigate
+  // function to navigate
   const navigate = useNavigate();
 
-  let [category, setCategory] = useState("all");
-  let [duration, setDuration] = useState("day");
-  let [trending, setTrending] = useState([]);
-  let [page, setPage] = useState(1);
-  
+  const [category, setCategory] = useState("all");
+  const [duration, setDuration] = useState("day");
+  const [trending, setTrending] = useState([]);
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
 
   const GetTrending = async () => {
     try {
-      const { data } = await axios.get(`/trending/${category}/${duration}`); 
+      const { data } = await axios.get(`/trending/${category}/${duration}?page=${page}`);
       // setTrending(data.results);
-      setTrending((prevState) =>[...prevState, ...data.results]);
-      setPage(page + 1);
+
+
+      if (data.results.length > 0) {
+        setTrending((prevState) => [...prevState, ...data.results]);
+        setPage(page + 1);
+      } else {
+        setHasMore(false);
+      }
     } catch (error) {
-      console.log("Error: ", error); 
+      console.log("Error: ", error);
+    }
+  };
+
+  const refreshHandler = () => {
+    if (trending.length === 0) {
+      GetTrending();
+    } else {
+      setPage(1);
+      setTrending([]);
+      GetTrending();
     }
   };
 
   useEffect(() => {
-    GetTrending();
+    refreshHandler();
   }, [category, duration]);
 
- 
   return trending.length ? (
     <div className="w-screen h-screen ">
       <div className=" w-full flex items-center px-[2vh ]">
@@ -46,24 +60,31 @@ const Trending = () => {
           Trending
         </h1>
         <TopNav />
-        <Dropdown  tile="category" options={["Movie", "Tv", "All"]} func={(e)=>setCategory(e.target.value.toLowerCase())} />
+        <Dropdown
+          tile="category"
+          options={["Movie", "Tv", "All"]}
+          func={(e) => setCategory(e.target.value.toLowerCase())}
+        />
         <div className="w-[2%]"></div>
-        <Dropdown tile="Duration" options={["Week", "Day"]} func={(e)=>setDuration(e.target.value.toLowerCase())} />
+        <Dropdown
+          tile="Duration"
+          options={["Week", "Day"]}
+          func={(e) => setDuration(e.target.value.toLowerCase())}
+        />
       </div>
 
-
-<InfiniteScroll
-  dataLength={trending.length}
-  next={GetTrending}
-  hasMore={true}
-  loader={<h4>Loading...</h4>}
->
-
- <Cards data={trending} />
-</InfiniteScroll>
-      
+      <InfiniteScroll
+        dataLength={trending.length}
+        next={GetTrending}
+        hasMore={hasMore}
+        loader={<h4>Loading...</h4>}
+      >
+        <Cards data={trending} />
+      </InfiniteScroll>
     </div>
-  ): <Loading />
+  ) : (
+    <Loading />
+  );
 };
 
 export default Trending;
